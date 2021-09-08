d0ed31764851c6ec4b6509453e1912cdcd9e1f01
liangxiaobin <1490556728@qq.com>
2021-09-08 14:24:55 +0800

    feat goroutine lookup

---
+++ pprof_v3/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime"
+)
+
+func init() {
+	runtime.SetMutexProfileFraction(1)
+	runtime.SetBlockProfileRate(1)
+}
+
+func main() {
+	http.HandleFunc("/lookup/heap", func(w http.ResponseWriter, r *http.Request) {
+		_ = pprofLookup(LookupHeap, os.Stdout)
+	})
+	http.HandleFunc("/lookup/block", func(rw http.ResponseWriter, r *http.Request) {
+		_ = pprofLookup(LookupBlock, os.Stdout)
+	})
+	http.HandleFunc("/lookup/goroutine", func(rw http.ResponseWriter, r *http.Request) {
+		_ = pprofLookup(LookupGoroutine, os.Stdout)
+	})
+	_ = http.ListenAndServe("0.0.0.0:6060", nil)
+}
