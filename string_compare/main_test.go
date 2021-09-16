3e4080ecc7daa00860bc7a608143d6957a822206
liangxiaobin <1490556728@qq.com>
2021-09-16 17:29:41 +0800

    feat string compare

---
+++ string_compare/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func BenchmarkTest1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Test1()
+	}
+}
+
+func BenchmarkTest2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Test2()
+	}
+}
